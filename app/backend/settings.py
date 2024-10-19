b2641b10ad9f7512709fcea57ee53d28b469e9aa
Najat Khoudrouf <nkhoudro@e2r6p5.1337.ma>
2024-10-19 15:16:49 +0100

    fix aut backend sing in and sing up use username password pass2 email

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -182,5 +182,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
--- app/base/serializers.py
+++ app/base/serializers.py
@@ -1,24 +1,34 @@
-from django.cont.auth.models import User
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class UserSerializer(serializers.ModelSerialiser):
-        class Meta:
-            model = User
-            field = ['username', 'email', 'password', 'password2']
-        
-        def create(self, validated_data):
-            user = User.objects.create_user(
-                validated_data['username'],
-                validated_data['email'],
-                validated_data['password']
-                validated_data['password2']
-            )
-            return user
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)  # For password confirmation
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')  # Remove password2 before creating user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.Charfield()
-    password = serializers.Charfield()
+    username = serializers.CharField()
+    password = serializers.CharField()
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
