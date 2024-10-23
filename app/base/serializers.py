139ce282933526a27a8d90c31a39e6b2a7607460
Najat Khoudrouf <nkhoudro@e2r9p5.1337.ma>
2024-10-23 19:25:25 +0100

    add firstnme and last name

--- app/base/serializers.py
+++ app/base/serializers.py
@@ -7,14 +7,32 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email','first_name','last_name',  'password', 'password2']
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True},
+            'first_name': {'write_only': True},
+            'last_name': {'write_only': True},
+            'email': {'write_only': True},
+            'username': {'write_only': True},
         }
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError("Passwords must match.")
+        elif User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError("Username already exists.")
+        elif User.objects.filter(email=data.get('email', '')).exists():
+            raise serializers.ValidationError("Email already exists.")
+        elif User.objects.filter(first_name=data.get('first_name', '')).exists():
+            raise serializers.ValidationError("first_name already exists.")
+        # elif len(data['password']) < 8:
+        #     raise serializers.ValidationError("Password must be at least 8 characters.")
+        # elif len(data['username']) < 5 or len(data['username']) > 20:
+        #     raise serializers.ValidationError("Username needs to be between 5 and 20 characters.")
+        # elif len(data['first_name']) < 2 or len(data['first_name']) > 20:
+        #     raise serializers.ValidationError("first_name needs to be between 2 and 20 characters.")
+        # elif len(data['last_name']) < 2 or len(data['last_name']) > 20:
+        #     raise serializers.ValidationError("last_name needs to be between 2 and 20 characters.")
         return data
 
     def create(self, validated_data):
@@ -22,7 +40,9 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),  # Use get() here
+            last_name=validated_data.get('last_name', '')
         )
         return user
 
@@ -30,10 +50,13 @@ class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
+class LogOutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
         token['username'] = user.username
-        token['email'] = user.email
         return token
--- app/base/views.py
+++ app/base/views.py
@@ -11,8 +11,10 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({"message": "User created successfully", 'access': str(refresh.access_token),
+                    'refresh': str(refresh),}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # User Login View (Using JWT for authentication)
